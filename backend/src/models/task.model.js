a3fa5a6c2180a4d39df4b0c8c355bc122554a7e7
Robert Pradilla <robertpradilla03@gmail.com>
2026-06-17 20:08:22 +0800

    add task model with schema definition for user tasks

---
+++ backend/src/models/task.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const taskSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    task: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    priority: {
+        type: String,
+        enum: ["low", "medium", "high"],
+        default: "medium",
+        required: true
+    },
+    tags: {
+        type: [string],
+        default: [],
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+},
+    {
+        timestamps: true
+    }
+);
+
+export const taskModel = mongoose.model("Task", taskSchema);
