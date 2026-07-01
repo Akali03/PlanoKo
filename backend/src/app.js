47b7099690a77e73757dead732b6aa61fc016325
Robert Pradilla <robertpradilla03@gmail.com>
2026-07-01 21:27:58 +0800

    feat: update CORS configuration to support dynamic frontend URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,11 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+     process.env.FRONTEND_URL,
+     
+  ],
   credentials: true
 }));
 app.use(express.json());
